0b267bb03eaa2132a9a15c4c55437bd362a96884
KyeongwooOh <ongdv94@gmail.com>
2022-07-27 19:04:58 +0900

    [UPDATE] Update
    회원가입, 로그인 구현.
    파이어스토어 연결은 했는데...조잡...
    이제 지갑 생성하고, 연결하는 로직 진행

--- js/fdb.js
+++ js/fdb.js
@@ -1,16 +1,32 @@
 async function addUser(userInfo) {
-  const { user_id, user_nm, user_passwrd, wallet_address = '' } = userInfo;
-  db.collection('users')
-    .add({
-      user_id,
-      user_nm,
-      user_passwrd,
-      wallet_address,
+  const { user_id, user_nm, user_pw, wallet_address = '' } = userInfo;
+
+  const result = await db.collection('users').add({
+    user_id,
+    user_nm,
+    user_pw,
+    wallet_address,
+  });
+  console.log(result);
+  return result;
+}
+
+async function loginUser(userInfo) {
+  try {
+    const { login_id, login_pw } = userInfo;
+    const result = await  db.collection('users').where('user_id', '==', login_id).where('user_pw', '==', login_pw).get()
+    result.forEach((item) => {
+        dd = item.data()
     })
-    .then((res) => {
-      console.log('Document written with ID: ', res);
-    })
-    .catch((err) => {
-      console.error('Error adding document: ', err);
-    });
+    const user = {
+        user_id:dd.user_id,
+        user_nm:dd.user_nm,
+        wallet_address: dd.wallet_address
+    }
+    return user;
+  } catch (error) {
+    return error;
+  }
+
+  return false;
 }
--- js/login.js
+++ js/login.js
@@ -1,8 +1,16 @@
 $(function() {
+    const sess = getCookie('user_info')
+    if(sess){
+        location.href="/"
+        return;
+    }
     var loginCond = false;
     const changeBtn = $("#change-btn")
     const signupForm = $('#signup-form')
     const loginForm = $("#login-form")
+
+    const loginBtn = loginForm.find('#login_btn');
+    const signupBtn = signupForm.find("#signup-btn")
 
     loginForm.show()
     signupForm.hide();
@@ -18,6 +26,30 @@ $(function() {
             signupForm.hide(350);
             loginForm.show(350)
         }
+    })
+
+    loginBtn.click(async function() {
+        const login_id = loginForm.find('#login_id').val()
+        const login_pw = loginForm.find('#login_pw').val()
         
+        const result = await loginUser({login_id, login_pw})
+        const t = JSON.stringify(result);
+        setCookie("user_info",t)
+    })
+
+    signupBtn.click(async function() {
+        const user_id = signupForm.find('#user_id').val()
+        const user_pw = signupForm.find('#user_pw').val()
+        const user_pw_check = signupForm.find('#user_pw_check').val()
+        const user_nm = signupForm.find('#user_nm').val()
+        const cond = user_pw === user_pw_check
+        if(!cond) {
+            return;
+        }
+        const result = await addUser({user_id, user_pw, user_nm});
+        if(result){
+            const t = JSON.stringify({user_id, user_pw, user_nm, wallet_address: ''});
+            setCookie("user_info",t)
+        }
     })
 })
